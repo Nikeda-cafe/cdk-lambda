24cf9867c4c132207a7dd9ab0e837a7998379ea6
nikeda <4104cafeaulait@gmail.com>
2025-06-28 11:04:16 +0900

    add: cdk sqs

--- lib/cdk-lamda-ts-stack.ts
+++ lib/cdk-lamda-ts-stack.ts
@@ -3,8 +3,10 @@ import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
 import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as sqs from 'aws-cdk-lib/aws-sqs'
 import * as path from 'path';
 import { LambdaDestination } from 'aws-cdk-lib/aws-s3-notifications';
+import { SqsEventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 
 const REPOSITORY_TOP = path.join(__dirname, '../')
 const PREFIX = 'cdk-lambda-ts-2';
@@ -21,6 +23,18 @@ export class CdkLamdaTsStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY,
     })
 
+    const dlqGrayscale = new sqs.Queue(this, `${PREFIX}-dlq-grayscale`, {
+      queueName: `${PREFIX}-dlq-grayscale`,
+    })
+    const queueGrayscale = new sqs.Queue(this, `${PREFIX}-queue-grayscale`, {
+      queueName: `${PREFIX}-queue-grayscale`,
+      deadLetterQueue:{
+        maxReceiveCount: 1,
+        queue: dlqGrayscale,
+      }
+    })
+
+    // lambda resize
     const resizeLambda = new NodejsFunction(this, `${PREFIX}-lambda-resize`, {
       functionName: `${PREFIX}-resize`,
       entry: path.join(REPOSITORY_TOP, 'lambdas/resize/src/index.ts'),
@@ -28,14 +42,34 @@ export class CdkLamdaTsStack extends cdk.Stack {
       runtime: lambda.Runtime.NODEJS_20_X,
       memorySize: 128,
       timeout: cdk.Duration.seconds(30),
+      environment: {
+        QUEUE_URL: queueGrayscale.queueUrl,
+      }
     })
     bucket.grantPut(resizeLambda);
     bucket.grantReadWrite(resizeLambda);
+    queueGrayscale.grantSendMessages(resizeLambda)
 
     bucket.addEventNotification(
       s3.EventType.OBJECT_CREATED,
       new LambdaDestination(resizeLambda),
       { prefix: 'original/' }
     )
+
+    // lambda grayscale
+    const grayscaleLambda = new NodejsFunction(this, `${PREFIX}-lambda-grayscale`, {
+      functionName: `${PREFIX}-grayscale`,
+      entry: path.join(REPOSITORY_TOP, 'lambdas/grayscale/src/index.ts'),
+      handler: 'handler',
+      runtime: lambda.Runtime.NODEJS_20_X,
+      memorySize: 128,
+      timeout: cdk.Duration.seconds(30),
+      environment: {
+        QUEUE_URL: queueGrayscale.queueUrl,
+      }
+    })
+    bucket.grantPut(grayscaleLambda);
+    bucket.grantReadWrite(grayscaleLambda);
+    grayscaleLambda.addEventSource(new SqsEventSource(queueGrayscale))
   }
 }
